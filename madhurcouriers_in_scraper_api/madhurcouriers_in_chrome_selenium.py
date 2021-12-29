e5d7aee3f833a1d86993c0b5fead41784b7a30ca
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2021-12-29 15:52:46 +0500

    2

--- madhurcouriers_in_scraper_api/madhurcouriers_in_chrome_selenium.py
+++ madhurcouriers_in_scraper_api/madhurcouriers_in_chrome_selenium.py
@@ -53,7 +53,7 @@ chrome_opt.add_argument('--disable-dev-sh--usage')
 
 url='https://www.madhurcouriers.in/CNoteTracking'
 
-for i in range(0,4):
+for i in range(0,2):
     
     print(datetime.datetime.now(), end=' ')
 
