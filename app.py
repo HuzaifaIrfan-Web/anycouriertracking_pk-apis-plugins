5be80eedbd6734d7fa1a53da9ee61051c9485b63
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2022-01-01 17:57:25 +0500

    deploy check

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ def index():
     return render_template('index.html')
 
 
-from madhurcouriers_in_scraper_api.app import madhurcouriers_in_track
+# from madhurcouriers_in_scraper_api.app import madhurcouriers_in_track
 
 from stcourier_scraper_api.app import stcourier_track
 
 
-app.add_url_rule('/track/madhurcouriers_in_scraper_api', 'madhurcouriers_in_track', madhurcouriers_in_track)
+# app.add_url_rule('/track/madhurcouriers_in_scraper_api', 'madhurcouriers_in_track', madhurcouriers_in_track)
 
 app.add_url_rule('/track/stcourier_scraper_api', 'stcourier_track', stcourier_track)
  
